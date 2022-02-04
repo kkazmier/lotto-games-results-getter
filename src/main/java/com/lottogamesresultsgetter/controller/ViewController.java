af01810d41a2a9d10d91873664aedc36b7317022
kkazmier <55816545+kkazmier@users.noreply.github.com>
2022-02-04 19:55:17 +0100

    Create ViewController.java

---
+++ src/main/java/com/lottogamesresultsgetter/controller/ViewController.java
@@ -0,0 +1,27 @@
+package com.lottogameresultsgetter.controller;
+
+import com.lottoresultsgetter.model.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+    private final Logger logger = LoggerFactory.getLogger(ViewController.class);
+
+    @GetMapping({"/", "/home"})
+    public String home(Model model) {
+        model.addAttribute("Lotto", new Result());
+        model.addAttribute("MiniLotto", new Result());
+        model.addAttribute("MultiMulti", new Result());
+        model.addAttribute("Kaskada", new Result());
+        model.addAttribute("Eurojackpot", new Result());
+        //model.addAttribute("Lotto", "Lotto");
+        logger.info("Attr: " + model.getAttribute("Lotto").toString());
+        return "home";
+    }
+
+
+}
