ef33de37a919fefae996c5052ee1915473f0aeb8
kkazm <s14005@pjwstk.edu.pl>
2022-02-04 15:07:52 +0100

    Initial commit.

---
+++ src/main/java/com/lottogamesresultsgetter/LottoGamesResultsGetterApplication.java
@@ -0,0 +1,13 @@
+package com.lottogamesresultsgetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoGamesResultsGetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottoGamesResultsGetterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lottogamesresultsgetter/LottoGamesResultsGetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lottogamesresultsgetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoGamesResultsGetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
